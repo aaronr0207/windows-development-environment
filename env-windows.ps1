30c613b4b76bbc41d532c5581db5b9abdff3a563
Aaron <aaronr0207@gmail.com>
2023-02-22 14:24:47 +0100

    Update env-windows.ps1

--- env-windows.ps1
+++ env-windows.ps1
@@ -33,107 +33,54 @@ Update-Environment-Path
 # Utils
 Get-Command -Module Microsoft.PowerShell.Archive
 
-#
-# Git
-#
+choco install adobereader -y
+choco install googlechrome -y
+choco install firefox -y
+choco install vcredist140 --version 14.34.31938 -y
+choco install google-drive-file-stream --version 56.0.11.2022 -y
+choco install 7zip.install -y
+choco install anydesk.install -y
+choco install git -y
+choco install vagrant -y
+choco install virtualbox -y
+choco install heidisql -y
+choco install hyper -y
+choco install logitech-options -y
+choco install notepadplusplus.install -y
+choco install phpstorm --version 2020.2.4 -y
+choco install putty -y
+choco install kitty -y
+choco install sublimetext4 -y
+choco install sublimemerge -y
+choco install tableplus -y
+choco install winscp -y
+choco install pdf24 -y
+choco install hostsman -y
+choco install spotify -y
+choco install postman -y
 
-choco install git --yes --params '/GitAndUnixToolsOnPath'
-choco install tortoisegit --yes
-Update-Environment-Path
-
-git config --global core.editor "code --wait"
-git config --global init.defaultBranch main
-
-# Aliases
-git config --global alias.pom 'pull origin main'
-git config --global alias.last 'log -1 HEAD'
-git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short --graph"
-git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
-git config --global alias.ammend "commit -a --amend"
-git config --global alias.everything "! git pull && git submodule update --init --recursive"
-git config --global alias.aliases "config --get-regexp alias"
-
-# PowerShell Tooling for Git
-Install-Module posh-git -Force -Scope CurrentUser
-Install-Module oh-my-posh -Force -Scope CurrentUser
-Set-Prompt
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
-Add-Content $PROFILE "`nImport-Module posh-git`nImport-Module oh-my-posh`nSet-PoshPrompt Paradox"
-
-
-# Font to support PowerShell Tooling:
-git clone https://github.com/ryanoasis/nerd-fonts.git  --depth 1
-cd nerd-fonts
-.\install.ps1
-cd ..\
-Write-Output 'Be sure to configure Windows Terminal fonts! Suggest using "fontFace": "MesloLGM NF"'
-
-#
-# AWS awscli
-#
-choco install awscli --yes
-Update-Environment-Path
-
-# Node
-choco install nodejs.install --yes
-Update-Environment-Path
-npm install --global --production npm-windows-upgrade
-npm-windows-upgrade --npm-version latest
-
-#
-# Docker
-# 
-
-# Hyper-V required for docker and other things
-Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All -NoRestart
-
-choco install docker-desktop --yes
 
 Update-Environment-Path
 
-#
-# Kubernetes
-#
-
-# choco install minikube --yes
-# choco install kubernetes-cli --yes
-
-# Note: VirtualBox sucks, see instructions here to run minikube: https://medium.com/@JockDaRock/minikube-on-windows-10-with-hyper-v-6ef0f4dc158c
-# TLDR: run with `minikube start --vm-driver hyperv --hyperv-virtual-switch "Primary Virtual Switch"`
-
-#
-# VS Code
-#
-
-choco install visualstudiocode --yes # includes dotnet
-Update-Environment-Path
-
-bash.exe vscode-extensions.sh
-
-# Windows Terminal
-choco install microsoft-windows-terminal --yes
-
-# File Management
-choco install beyondcompare --yes
-choco install 7zip --yes
-
-# Media Viewers
-choco install irfanview --yes
-choco install vlc --yes
-
-# Browsers
-choco install googlechrome --yes
-choco install firefox --yes
-
-# Misc
-choco install sysinternals --yes
-choco install procexp --yes
-choco install firacode --yes # See https://www.youtube.com/watch?v=KI6m_B1f8jc
-choco install everything --yes
-
-Update-Environment-Path
+# Poner el tema oscuro
+start "" "C:\Windows\Resources\Themes\themeB.theme"
 
 # Windows Subsystem for Linux
-wsl --install
+# wsl --install
 
-Write-Output "Finished! Run `choco upgrade all` to get the latest software"
+# Homestead basic setup
+
+cd ~
+mkdir workspace
+mkdir compartida
+git clone https://github.com/laravel/homestead.git homestead
+cd homestead
+git checkout release
+bash init.sh
+vagrant plugin install vagrant-winnfsd
+
+# Si se va a usar el sistema de archivos nfs hay que ejecutar los permisos (nota keep)
+# La configuración del yaml hay que hacerla manualmente de momento
+
+
+Write-Output "Finalizado! Ejecuta `choco upgrade all` si necesitas actualizar el software (cuidado con la versión de phpstorm)"
